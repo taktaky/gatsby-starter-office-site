bbccb0cc22d8d0e47163c1c06bda9a7a7e9703a0
tack <taktaky@gmail.com>
2020-10-29 22:50:15 +0900

    ヘッダーとメニューを調整

--- src/components/header.js
+++ src/components/header.js
@@ -9,12 +9,18 @@ const Header = ({ siteTitle }) => (
     .bg-header-color {
       background-color: #F000A0;
       color: white;
+      margin: 5px 0px 5px 0px;
+      padding: 5px 5px 5px;
+    }
+    .container {
+      margin: 5px;
+      padding: 5px;
     }
     .text-title {
       font-size: 3.5rem;
       color: white;
-      margin: 5px 5px;
-      padding: 5px 2px;
+      margin: 0px;
+      padding: 0px;
     }
     .text-sub {
       font-size: 1rem;
@@ -26,7 +32,7 @@ const Header = ({ siteTitle }) => (
     </style>
 
     <header className="bg-header-color">
-      <Container>
+      <Container className="container">
         <Row>
           <Col xs={12} sm={12} md={8} lg={8}>
             <Link to="/" className="text-title"><b>{siteTitle}</b></Link>
@@ -34,7 +40,7 @@ const Header = ({ siteTitle }) => (
           <Col xs={12} sm={12} md={4} lg={4}>
             <div style={{textAlign: "right"}}>
               <Link to="/" className="text-sub">
-                <b>〒XXX-XXXX&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
+                <b>〒XXX-XXXX&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br/>
                 XX県XX市XX X-XX-XX</b>
               </Link>
               <br />
--- src/components/menu.js
+++ src/components/menu.js
@@ -6,37 +6,43 @@ const Menu = () => (
   <>
     <style type="text/css">
     {`
+    .row-menu {
+      padding: 2px 10px 0px 3px;
+    }
+    .col-menu {
+      padding: 2px 2px 0px;
+    }
     .btn-menu {
       font-size: 1.3rem;
       background-color: #F000A0;
       color: white;
-      margin: 3px 5px;
-      padding: 5px 5px;
+      margin: 0px 0px 2px;
+      padding: 2px 0px;
       //width: 8rem;
       outline-color: #F000A0;
     }
     `}
     </style>
 
-    <div>
+    <div >
       <Container>
-        <Row>
-          <Col xs={12} sm={4} md={4} lg={2}>
+        <Row className="row-menu">
+          <Col xs={12} sm={4} md={4} lg={2} className="col-menu">
             <Button href="/" className="btn-menu" block variant="outline-danger"><b>ホーム</b></Button>
           </Col>
-          <Col xs={12} sm={4} md={4} lg={2}>
+          <Col xs={12} sm={4} md={4} lg={2} className="col-menu">
             <Button to="/02" className="btn-menu" block variant="outline-danger"><b>診療案内</b></Button>
           </Col>
-          <Col xs={12} sm={4} md={4} lg={2}>
+          <Col xs={12} sm={4} md={4} lg={2} className="col-menu">
             <Button to="/03" className="btn-menu" block variant="outline-danger"><b>院内紹介</b></Button>
           </Col>
-          <Col xs={12} sm={4} md={4} lg={2}>
+          <Col xs={12} sm={4} md={4} lg={2} className="col-menu">
             <Button to="/04" className="btn-menu" block variant="outline-danger"><b>スタッフ紹介</b></Button>
           </Col>
-          <Col xs={12} sm={4} md={4} lg={2}>
+          <Col xs={12} sm={4} md={4} lg={2} className="col-menu">
             <Button to="/05" className="btn-menu" block variant="outline-danger"><b>アクセス</b></Button>
           </Col>
-          <Col xs={12} sm={4} md={4} lg={2}>
+          <Col xs={12} sm={4} md={4} lg={2} className="col-menu">
             <Button to="/06" className="btn-menu" block variant="outline-danger"><b>お問い合わせ</b></Button>
           </Col>
         </Row>
